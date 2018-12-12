1ed0309a1693d457dc00709cf6e96d3f886be9b4
Peter Tribe <ptribe@pnimedia.com>
2018-12-12 00:22:23 -0800

    Update: mac example

--- Veldrid.Mac/ViewController.cs
+++ Veldrid.Mac/ViewController.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using AppKit;
-using CoreVideo;
+using Foundation;
 using Veldrid;
 
 namespace VeldridNSViewExample
 {
-    public partial class ViewController : NSSplitViewController
+    [Register("ViewController")]
+    public class ViewController : NSSplitViewController
     {
-        private readonly GraphicsDevice _gd;
-        private Swapchain _sc;
-        private CommandList _cl;
+        private VeldridView _veldridView;
+        private CommandList _commandList;
+
         private int _frameIndex = 0;
         private RgbaFloat[] _clearColors =
         {
@@ -21,71 +22,53 @@ namespace VeldridNSViewExample
             new RgbaFloat(0.8f, 0.1f, 0.3f, 1f),
             new RgbaFloat(0.8f, 0.1f, 0.9f, 1f),
         };
-        private bool _resized;
-        private (uint Width, uint Height) _size;
         private readonly int _frameRepeatCount = 20;
 
         public ViewController(IntPtr handle) : base(handle)
         {
-            _frameIndex = new Random().Next(0, _clearColors.Length * _frameRepeatCount);
-            _gd = AppGlobals.Device;
         }
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
 
-            var ss = SwapchainSource.CreateNSView(View.Handle);
-            SwapchainDescription scDesc = new SwapchainDescription(
-                ss, (uint)View.Frame.Width, (uint)View.Frame.Height, null, true, true);
-            _sc = AppGlobals.Device.ResourceFactory.CreateSwapchain(scDesc);
-            _cl = AppGlobals.Device.ResourceFactory.CreateCommandList();
+            var graphicsDeviceOptions = new GraphicsDeviceOptions(false, null, false, ResourceBindingModel.Improved, true, true);
 
-            // To render at a steady rate, we create a display link which will invoke our Render function.
-            CVDisplayLink displayLink = new CVDisplayLink();
-            displayLink.SetOutputCallback(HandleDisplayLinkOutputCallback);
-            displayLink.Start();
+            _veldridView = new VeldridView(GraphicsBackend.Metal, graphicsDeviceOptions)
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false
+            };
+            View.AddSubview(_veldridView);
+            _veldridView.LeftAnchor.ConstraintEqualToAnchor(View.LeftAnchor, 16).Active = true;
+            _veldridView.RightAnchor.ConstraintEqualToAnchor(View.RightAnchor, -16).Active = true;
+            _veldridView.TopAnchor.ConstraintEqualToAnchor(View.TopAnchor, 16).Active = true;
+            _veldridView.BottomAnchor.ConstraintEqualToAnchor(View.BottomAnchor, -16).Active = true;
+
+            _veldridView.DeviceReady += VeldridView_DeviceReady;
+            _veldridView.Resized += VeldridView_Resized;
+            _veldridView.Rendering += VeldridView_Rendering;
         }
 
-        private void Render()
+        void VeldridView_DeviceReady()
         {
-            // If we've detected a resize event, we handle it now.
-            if (_resized)
-            {
-                _resized = false;
-                _sc.Resize(_size.Width, _size.Height);
-            }
+            _commandList = _veldridView.GraphicsDevice.ResourceFactory.CreateCommandList();
+        }
 
-            // Each frame, we clear the Swapchain's color target.
-            // Several different colors are cycled.
-            _cl.Begin();
-            _cl.SetFramebuffer(_sc.Framebuffer);
-            _cl.ClearColorTarget(0, _clearColors[(_frameIndex / _frameRepeatCount)]);
-            _cl.End();
-            _gd.SubmitCommands(_cl);
-            _gd.SwapBuffers(_sc);
+        void VeldridView_Resized()
+        {
+        }
 
-            // Do some math to loop our color picker index.
+        void VeldridView_Rendering()
+        {
+            _commandList.Begin();
+            _commandList.SetFramebuffer(_veldridView.MainSwapchain.Framebuffer);
+            _commandList.ClearColorTarget(0, _clearColors[_frameIndex / _frameRepeatCount]);
+            _commandList.End();
+            _veldridView.GraphicsDevice.SubmitCommands(_commandList);
+            _veldridView.GraphicsDevice.SwapBuffers(_veldridView.MainSwapchain);
+
             _frameIndex = (_frameIndex + 1) % (_clearColors.Length * _frameRepeatCount);
         }
 
-        private CVReturn HandleDisplayLinkOutputCallback(
-            CVDisplayLink displayLink,
-            ref CVTimeStamp inNow,
-            ref CVTimeStamp inOutputTime,
-            CVOptionFlags flagsIn,
-            ref CVOptionFlags flagsOut)
-        {
-            Render();
-            return CVReturn.Success;
-        }
-
-
-        public override void ViewDidLayout()
-        {
-            base.ViewDidLayout();
-            _resized = true;
-            _size = ((uint)View.Frame.Width, (uint)View.Frame.Height);
-        }
     }
 }
